fc31faa3f98826c57121d46a44d072c2ad96822e
Ubuntu <qamarali@Ubuntu4cpu16gbDLT.p5lvj3144vuezjqukm2remdf4b.gx.internal.cloudapp.net>
2021-02-07 15:45:50 +0000

    Added code for API

--- fabric-api2.1/app.js
+++ fabric-api2.1/app.js
@@ -1,4 +1,14 @@
+const bodyParser = require('body-parser');
+const http = require('http')
+const util = require('util');
+const express = require('express')
+const app = express();
+const constants = require('./config/constants.json')
 
-const chalk = require('chalk');
-const claimQuery = require('./src/QueryClaimData')
-console.log(chalk.red(claimQuery('claimId3')))
+app.get('',(req,res)=>{
+    res.render('',{'title':'param','created':'qamar'})
+})
+
+app.listen(3000,()=>{console.log('running server')
+})
+console.log('Hello')

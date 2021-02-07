c7a54e301eac2219058a8ebbc84aedf461b28374
Ubuntu <qamarali@Ubuntu4cpu16gbDLT.p5lvj3144vuezjqukm2remdf4b.gx.internal.cloudapp.net>
2021-02-07 02:21:39 +0000

    Intial commit

---
+++ fabric-api2.1/app.js
@@ -0,0 +1,4 @@
+
+const chalk = require('chalk');
+const claimQuery = require('./src/QueryClaimData')
+console.log(chalk.red(claimQuery('claimId3')))
---
+++ fabric-api2.1/src/QueryClaimData.js
@@ -0,0 +1,10 @@
+'use strict';
+const { Contract } = require('fabric-contract-api');
+
+const getClaimDataByClaimId = function(claimId)
+{
+
+    return claimId
+}
+
+module.exports=  getClaimDataByClaimId
